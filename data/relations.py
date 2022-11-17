5711e34741061ff8e0d27712e94ac1460c5b2deb
randwas12 <yaroslav.filatov@quantori.academy>
2022-11-17 20:04:56 +0600

    Revert "__repr__ methods removed"
    This reverts commit e8f0cd5c2c35bb94f880500b7c4f9c8bbf16cff3.

--- data/relations.py
+++ data/relations.py
@@ -3,15 +3,19 @@ from data.schemes import DNABase, RNACodon
 
 def get_rna_to_rnd_relation(session) -> dict:
     dna_bases = session.query(DNABase).all()
+
     relations = {
         dna_base.base: dna_base.related_rna_base.base for dna_base in dna_bases
     }
+
     return relations
 
 
 def get_rna_codon_to_amino_acid_relation(session) -> dict:
     rna_codons = session.query(RNACodon).all()
+
     relations = {
         codon.sequence: codon.amino_acid.sequence for codon in rna_codons
     }
+
     return relations
