15a7df10e3c0818da94490a632a05773113c8e2e
randwas12 <yaroslav.filatov@quantori.academy>
2022-12-03 01:20:00 +0600

    imports refactored & shortened var names

--- services/application/app/genome.py
+++ services/application/app/genome.py
@@ -3,27 +3,32 @@ from typing import Union
 
 from sqlalchemy.exc import SQLAlchemyError
 
-from app.data.relations import get_rna_to_rnd_relation, get_rna_codon_to_amino_acid_relation
+from app.data.relations import (
+    get_rna_codon_to_amino_acid_relation,
+    get_rna_to_rnd_relation
+)
 
 
-def convert_dna_to_rna(dna_sequence: str, session) -> Union[str, None]:
+def convert_dna_to_rna(dna: str, session) -> Union[str, None]:
     try:
         relation = get_rna_to_rnd_relation(session)
-        rna_sequence = ''.join(relation.get(char) for char in dna_sequence)
-        return rna_sequence
+        rna = ''.join(relation.get(char) for char in dna)
+        return rna
     except SQLAlchemyError as orm_error:
         logging.error(orm_error)
     except KeyError as key_error_exception:
         logging.error(key_error_exception)
 
 
-def convert_rna_to_amino_acid(rna_sequence: str, session) -> Union[str, None]:
+def convert_rna_to_amino_acid(rna: str, session) -> Union[str, None]:
     try:
         relation = get_rna_codon_to_amino_acid_relation(session)
-        sequence_size = 3 * (len(rna_sequence) // 3 - 1) + 1
-        codons = tuple(rna_sequence[index: index + 3] for index in range(0, sequence_size, 3))
-        amino_acid_sequence = ''.join(relation.get(codon, '.') for codon in codons)
-        return amino_acid_sequence
+        sequence_size = 3 * (len(rna) // 3 - 1) + 1
+        codons = tuple(
+            rna[index: index + 3] for index in range(0, sequence_size, 3)
+        )
+        amino_acid = ''.join(relation.get(codon, '.') for codon in codons)
+        return amino_acid
     except SQLAlchemyError as orm_error:
         logging.error(orm_error)
     except KeyError as key_error_exception:
